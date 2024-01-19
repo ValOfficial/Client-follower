3d991164f73f52bb50ba6722bfbee48afb8a92a5
Vladimir <156911659+ValOfficial@users.noreply.github.com>
2024-01-19 21:20:37 +0100

    Add files via upload

---
+++ t.bat
@@ -0,0 +1,174 @@
+@echo off
+:loop
+color 1
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh                                                                                
+timeout /nobreak /t 1 >nul
+color 2
+cls
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh 
+timeout /nobreak /t 1 >nul
+color 3
+cls
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh 
+timeout /nobreak /t 1 >nul
+color 4
+cls
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh 
+timeout /nobreak /t 1 >nul
+cls
+color 5
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh                                                                                
+timeout /nobreak /t 1 >nul
+color 6
+cls
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh 
+timeout /nobreak /t 1 >nul
+color 7
+cls
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh 
+timeout /nobreak /t 1 >nul
+color 8
+cls
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh 
+timeout /nobreak /t 1 >nul
+color 9
+cls
+echo XXXXXXX       XXXXXXX            444444444          SSSSSSSSSSSSSSS      hhhhhhh             
+echo X:::::X       X:::::X           4::::::::4        SS:::::::::::::::S     h:::::h             
+echo X:::::X       X:::::X          4:::::::::4       S:::::SSSSSS::::::S     h:::::h             
+echo X::::::X     X::::::X         4::::44::::4       S:::::S     SSSSSSS     h:::::h             
+echo XXX:::::X   X:::::XXX        4::::4 4::::4       S:::::S                  h::::h hhhhh       
+echo    X:::::X X:::::X          4::::4  4::::4       S:::::S                  h::::hh:::::hhh    
+echo     X:::::X:::::X          4::::4   4::::4        S::::SSSS               h::::::::::::::hh  
+echo      X:::::::::X          4::::444444::::444       SS::::::SSSSS          h:::::::hhh::::::h 
+echo      X:::::::::X          4::::::::::::::::4         SSS::::::::SS        h::::::h   h::::::h
+echo     X:::::X:::::X         4444444444:::::444            SSSSSS::::S       h:::::h     h:::::h
+echo    X:::::X X:::::X                  4::::4                   S:::::S      h:::::h     h:::::h
+echo XXX:::::X   X:::::XXX               4::::4                   S:::::S      h:::::h     h:::::h
+echo X::::::X     X::::::X               4::::4       SSSSSSS     S:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             44::::::44     S::::::SSSSSS:::::S      h:::::h     h:::::h
+echo X:::::X       X:::::X             4::::::::4     S:::::::::::::::SS       h:::::h     h:::::h
+echo XXXXXXX       XXXXXXX             4444444444      SSSSSSSSSSSSSSS         hhhhhhh     hhhhhhh 
+timeout /nobreak /t 1 >nul
+cls
+goto loop
